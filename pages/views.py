f9b52832bad6947988faafefcb1651e27b131524
dule <dule@dules-MacBook-Pro.local>
2023-05-31 03:00:28 +0200

    templates

--- pages/views.py
+++ pages/views.py
@@ -2,8 +2,14 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
-def home_view(*args,**kwargs):
-    return HttpResponse("<h1>hello</h1>")
+def home_view(request,*args,**kwargs):
+    print(request.user)
+    #return HttpResponse("<h1>hello</h1>")
+    return render(request,"home.html",{})
 
-def contact_view(*args,**kwargs):
-    return HttpResponse("<h1>contact</h1>")
+def contact_view(request,*args,**kwargs):
+    my_context = {
+        "text":"±test text iz konteksta±",
+        "lista":[1,2,3]
+    }
+    return render(request,"contact.html",my_context)
