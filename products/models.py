30c9d1bb721907b38e1aec3dc5dbc5eaddc15a06
dule <dule@dules-MacBook-Pro.local>
2023-06-03 02:41:55 +0200

    dynamic url

--- products/models.py
+++ products/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.urls import reverse
 # Create your models here.
 class Product(models.Model):
 
@@ -8,3 +8,7 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=1000,decimal_places=2)
     summary = models.TextField(blank=False ,null = False)
     featured = models.BooleanField(default=False)
+    
+    def get_absolute_url(self):
+        return reverse("product_detail",kwargs={"my_id":self.id})
+        #return f"/products/{self.id}"
--- products/views.py
+++ products/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404,redirect
 from .models import Product
 from .forms import ProductForm,RawProductForm
 
 # Create your views here.
 def product_detail_view(request):
-    obj = Product.objects.get(id=1)
+    obj = Product.objects.get(id=7)
     context = {
         "object": obj
     }
@@ -33,3 +33,25 @@ def product_create_view(request):
         "form":form
     }
     return render(request, "products/product_create.html", context)
+
+def dynamic_lookup_view(request,my_id):
+    #obj = Product.objects.get(id=my_id)
+    obj = get_object_or_404(Product,id=my_id)
+    context = {"object":obj}
+    return render(request,"products/product_detail.html",context)
+
+def product_delete_view(request,my_id):
+    #obj = Product.objects.get(id=my_id)
+    obj = get_object_or_404(Product,id=my_id)
+    if request.method == "POST":
+        obj.delete()
+        return redirect('/')
+    context = {"object":obj}
+    return render(request,"products/product_delete.html",context)
+
+def product_list_view(request):
+    queryset = Product.objects.all()
+    context = {
+        "object_list":queryset
+    }
+    return render(request,"products/product_list.html",context)
